e89ba51010cf33fb6a116c12cec64648b75711bb
Alexandru Punga <alexandru.punga@atomate.net>
2021-02-22 13:03:25 +0200

    Generic models

--- db/models.py
+++ db/models.py
@@ -1,25 +1,23 @@
-from typing import Type
+from typing import TypeVar, Generic, Type
 from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
 from .schemas import UserBase, GameBase, QuestionBase, AnswerBase
 
+modelType = TypeVar("modelType", bound=BaseModel)
 
-class Fillable:
+
+class Fillable(Generic[modelType]):
     id = Column("id", Integer, primary_key=True)
 
-    @property
-    def schema(self) -> Type[BaseModel]:
-        return BaseModel
-
-    def fill(self, schema: schema):
+    def fill(self, schema: Type[modelType]):
         for key, value in schema.dict().items():
             if value is not None:
                 self.__setattr__(key, value)
 
 
-class User(Fillable, Base):
+class User(Fillable[UserBase], Base):
     __tablename__ = "users"
 
     firstname = Column("firstname", String(255), nullable=False)
@@ -30,41 +28,25 @@ class User(Fillable, Base):
     is_super = Column("is_super", Boolean, default=False, nullable=False)
     games = relationship("Game", back_populates="player")
 
-    @property
-    def schema(self) -> Type[UserBase]:
-        return UserBase
 
-
-class Game(Fillable, Base):
+class Game(Fillable[GameBase], Base):
     __tablename__ = "games"
 
     player_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     player = relationship("User", back_populates="games")
 
-    @property
-    def schema(self) -> Type[GameBase]:
-        return GameBase
 
-
-class Question(Fillable, Base):
+class Question(Fillable[QuestionBase], Base):
     __tablename__ = "questions"
 
     text = Column("text", Text, nullable=False)
     answers = relationship("Answer", back_populates="question")
     correct_answer = relationship("Answer")
 
-    @property
-    def schema(self) -> Type[QuestionBase]:
-        return QuestionBase
 
-
-class Answer(Fillable, Base):
+class Answer(Fillable[AnswerBase], Base):
     __tablename__ = "answers"
 
     text = Column("text", Text, nullable=False)
     question_id = Column(Integer, ForeignKey('questions.id'), nullable=False)
     question = relationship("Question", back_populates="answers")
-
-    @property
-    def schema(self) -> Type[AnswerBase]:
-        return AnswerBase
