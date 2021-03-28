a897c988047986a296f7915189a2091f1506f077
Sanchous98 <45293316+sanchous98@users.noreply.github.com>
2021-03-28 23:40:33 +0300

    Update README.md

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import json
-from typing import Union, TypeVar, Generic, Type
-
 from api import api
 from db import models
 from fastapi import FastAPI
@@ -15,19 +12,3 @@ except FileExistsError:
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI(version=getenv("VERSION", "dev"), debug=getenv("DEBUG", False))
 app.include_router(api.router, prefix="/api", tags=["API"])
-
-
-anyType = TypeVar("anyType")
-
-
-class X(Generic[anyType]):
-    def __init__(self, value: anyType):
-        self.value = value
-
-    def get(self) -> anyType:
-        return self.value
-
-
-var = 10
-a: anyType = 'aaa'
-a: anyType = X(int).get()
